c4bab471eaa4af39181758b84b668a87ad8d09cd
ghassarsa <ppziensxhu@gmail.com>
2025-09-09 19:54:23 +0700

    Eliminate React integration from Laravel

--- resources/js/Pages/Menu.jsx
+++
@@ -1,8 +0,0 @@
-export default function Menu({ menus }) {
-  return (
-    <div>
-      <h1>Daftar Menu</h1>
-      {menus.map(menu => <p key={menu.id}>{menu.nama_hidangan}</p>)}
-    </div>
-  );
-}
